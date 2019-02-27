f943453b8e552ccb452320480595489f47aa7943
Udhay Sankar <udhaysankarv@gmail.com>
2019-02-27 19:03:52 +0530

    Changes to be committed: 	modified:   Leetcode_5.c

---
+++ Leetcode_5.c
@@ -0,0 +1,14 @@
+//Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+#include<stdio.h>
+#define MAX 1000;
+char* longestPalindrome(char* s) {
+
+}
+int main() {
+  char string[MAX],answer[];
+  printf("\nEnter your string:")
+  scanf("%s",&string);
+  answer[]=longestPalindrome(string);
+  printf("\nThe longest palindrome is %s", )
+  return 0;
+}
